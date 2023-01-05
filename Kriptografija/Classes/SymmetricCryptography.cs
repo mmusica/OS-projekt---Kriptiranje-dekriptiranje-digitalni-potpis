3a86b0ed3822f6e412a5f1cdf91755d9759ecc14
Marko Mušica <markomusica@gmail.com>
2023-01-05 10:38:13 +0100

    Refactored code

--- Kriptografija/Classes/SymmetricCryptography.cs
+++ Kriptografija/Classes/SymmetricCryptography.cs
@@ -33,33 +33,26 @@ namespace Kriptografija.Classes
             if (IV == null || IV.Length <= 0)
                 throw new ArgumentNullException("Problem s IV");
             byte[] encrypted;
-
-            // Create an Aes object
-            // with the specified key and IV.
+         
             using (Aes symAesAlgorithm = Aes.Create())
             {
                 symAesAlgorithm.Key = Key;
                 symAesAlgorithm.IV = IV;
-               
-                // Create an encryptor to perform the stream transform.
                 ICryptoTransform encryptor = symAesAlgorithm.CreateEncryptor(symAesAlgorithm.Key, symAesAlgorithm.IV);
 
-                // Create the streams used for encryption.
                 using (MemoryStream msEncrypt = new MemoryStream())
                 {
                     using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                     {
                         using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
                         {
-                            //Write all data to the stream.
+                           
                             swEncrypt.Write(textToEncrypt);
                         }
                         encrypted = msEncrypt.ToArray();
                     }
                 }
-            }
-
-            // Return the encrypted bytes from the memory stream.
+            }    
             return encrypted;
         }
         public static string DecryptStringFromBytes_Aes(byte[] cipherText)
@@ -70,31 +63,21 @@ namespace Kriptografija.Classes
                 throw new ArgumentNullException("Key");
             if (IV == null || IV.Length <= 0)
                 throw new ArgumentNullException("IV");
-
-            // Declare the string used to hold
-            // the decrypted text.
             string plaintext = null;
 
-            // Create an Aes object
-            // with the specified key and IV.
             using (Aes aesAlg = Aes.Create())
             {
                 aesAlg.Key = Key;
                 aesAlg.IV = IV;
 
-                // Create a decryptor to perform the stream transform.
                 ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-                // Create the streams used for decryption.
                 using (MemoryStream msDecrypt = new MemoryStream(cipherText))
                 {
                     using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                     {
                         using (StreamReader srDecrypt = new StreamReader(csDecrypt))
                         {
-
-                            // Read the decrypted bytes from the decrypting stream
-                            // and place them in a string.
                             plaintext = srDecrypt.ReadToEnd();
                         }
                     }
